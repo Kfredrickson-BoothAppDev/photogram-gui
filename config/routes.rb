76cd1e952efcd387a8d580447c9436819ccf7a66
Kyle Fredrickson <kyle.fredrickson@chicagobooth.edu>
2023-05-02 03:36:58 +0000

    Photos

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ get("/users", {:controller => "users", :action => "index" })
 
 get("/users/:path_username", {:controller => "users", :action => "show" })
 
+get("/photos", { :controller => "photos", :action => "index"})
+
+get("/photos/:path_id", {:controller => "photos", :action => "show" })
+
+
 end
